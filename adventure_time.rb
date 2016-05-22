560356b7f9f1a02c7da371775719b32905647cb6
Ana L Huerta Huerta <anahuerta128@gmail.com>
2016-05-22 15:19:20 -0400

    After the train  ride

--- adventure_time.rb
+++ adventure_time.rb
@@ -5,45 +5,42 @@ number_of_players = $stdin.gets.chomp.to_i
 
 class Player
   attr_reader :name
+  attr_accessor :character
 
   def initialize(name)
     @name = name
   end
-
 end
 
 players = []
+
+class Character
+  attr_reader :name
+  
+  def initialize(name)
+  	@name = name
+  end
+end
+
+c1 = Character.new('Jake')
+c2 = Character.new('Finn')
+c3 = Character.new('Marceline')
+c4 = Character.new('Princess Bubblegum')
+
+characters = [c1, c2, c3, c4]
 
 number_of_players.times do |i|
   puts "What is the name for player number #{i + 1}?"
   print "< "
   name_of_a_player = $stdin.gets.chomp
-  players << Player.new(name_of_a_player)
+  player = Player.new(name_of_a_player)
+  players << player 
+
+  # TODO -> give each player a random character here
 end
 
 players.each do |player|
   puts player.name
+  puts player.character.name
 end
 
-class Character #< Player
-  @@character_names = ["Finn", "Jake"]
-
-# class method that creates all the characters
-  def self.create_characters
-
-  end
-
-# class method that returns a random character
-  def self.random_character
-
-  end
-
-  def initialize
-
-  end
-end
-
-def character(characters_name)
-	val = @@characters[characters_name]
-	return val
-end
