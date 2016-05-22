31c6f47d37ea677b6bda83444cd7d4faecd66593
Ana L Huerta Huerta <anahuerta128@gmail.com>
2016-05-22 17:32:34 -0400

    players can choose a character

--- adventure_time.rb
+++ adventure_time.rb
@@ -35,9 +35,15 @@ number_of_players.times do |i|
   name_of_a_player = $stdin.gets.chomp
   player = Player.new(name_of_a_player)
   players << player 
-
-  # TODO -> give each player a random character here
+  
+  puts "Which character do you want, #{player.name}? (choose a number)"
+  puts "1-Jake, 2-Finn, 3-Marceline, 4-Princess Bubblegum"
+  print "< "
+  player_number = $stdin.gets.chomp.to_i
+  
+  player.character = characters[player_number - 1]
 end
+
 
 players.each do |player|
   puts player.name
